f606d7ec73d602828a49268b12c48aeccfb94f22
alexgonca <alexandre.reportagem@gmail.com>
2018-09-14 14:43:05 -0400

    add region queues to config file and prepare_producer.sh

--- url_facebook_stats_producer.py
+++ url_facebook_stats_producer.py
@@ -4,16 +4,20 @@ import boto3
 import json
 import configparser
 import os
+import logging
 
-if __name__ == '__main__':
+
+def main():
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
     # Connect to Postgres server.
     config = configparser.ConfigParser()
     config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
     conn = psycopg2.connect(host=config['database']['host'],
-                            dbname=config['database']['dbname'],
+                            dbname=config['database']['db_name'],
                             user=config['database']['user'],
                             password=config['database']['password'])
     cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+    logging.info('Connected to database %s', config['database']['db_name'])
 
     # Retrieve AWS credentials and connect to Simple Queue Service (SQS).
     cur.execute("""select * from aws_credentials;""")
@@ -21,60 +25,91 @@ if __name__ == '__main__':
     aws_session = boto3.Session(
         aws_access_key_id=aws_credential['aws_access_key_id'],
         aws_secret_access_key=aws_credential['aws_secret_access_key'],
-        region_name=aws_credential['region_name']
+        region_name=config['aws']['region_queues']
     )
     sqs = aws_session.resource('sqs')
+    logging.info('Connected to AWS in %s', aws_credential['region_name'])
 
     # Connect to queue for Twitter credentials.
-    credentials_queue = sqs.get_queue_by_name(QueueName='facebook-credentials')
+    credentials_queue = sqs.get_queue_by_name(QueueName='facebook_credentials')
     credentials_queue.purge()
     # Enqueue all Twitter credentials available on the database.
-    cur.execute("""select * from facebook_credentials;""")
+    cur.execute("""select * from facebook_user_credentials;""")
     credentials = cur.fetchall()
     for credential in credentials:
-        message_body = json.dumps(credential)
-        credentials_queue.send_message(MessageBody=message_body)
-
-    ids_queue = sqs.get_queue_by_name(QueueName='facebook-urls')
-    ids_queue.purge()
-
-    cur.execute("SELECT * FROM query WHERE status = 'FACEBOOK'")
-    queries = cur.fetchall()
-    for query in queries:
-        cur.execute("""select url, min(created_at)::text as created_at, max(twitter) as twitter, max(google) as google
-                        from
-                        (select expanded_url as url,
-                           min(created_at)::date as created_at,
-                           1 as twitter,
-                           0 as google
-                        from tweet_url
-                        where query_alias = '{0}' and position('.facebook.com' in expanded_url) = 0
-                        group by expanded_url
-                        union all
-                        select url,
-                          min(case when date is not NULL then date else initial_date end) as created_at,
-                          0 as twitter, 1 as google
-                        from search_result
-                        where query_alias = '{0}' and position('facebook.com' in domain) = 0
-                        group by url) subquery
-                        where not exists (select *
-                          from facebook_url_stats
-                          where facebook_url_stats.query_alias = '{0}' and 
-                          subquery.url = facebook_url_stats.url)
-                        group by url;""".format(query['query_alias']))
-        no_more_results = False
-        while not no_more_results:
-            ids = cur.fetchmany(size=100)
-            if len(ids) == 0:
-                no_more_results = True
-            else:
-                message_body = {
-                    'query_alias': query['query_alias'],
-                    'urls': [{'url': id['url'],
-                              'created_at': id['created_at'],
-                              'twitter': id['twitter'],
-                              'google': id['google']} for id in ids]
+        credential_user = {
+            'type': 'user',
+            'credentials': [
+                {
+                    'id': credential['email'],
+                    'access_token': credential['access_token']
                 }
-                ids_queue.send_message(MessageBody=json.dumps(message_body))
+            ]
+        }
+        message_body = json.dumps(credential_user)
+        credentials_queue.send_message(MessageBody=message_body)
+    # todo : maybe break app credentials in groups with 3 credentials per message.
+    credential_app = {
+        'type': 'app',
+        'credentials': []
+    }
+    cur.execute("""select * from facebook_app_credentials;""")
+    credentials = cur.fetchall()
+    for credential in credentials:
+        credential_app['credentials'].append({
+            'id': credential['app_name'],
+            'access_token': "{}|{}".format(credential['app_id'], credential['app_secret'])
+        })
+    message_body = json.dumps(credential_app)
+    credentials_queue.send_message(MessageBody=message_body)
+    logging.info('Enqueued all facebook credentials')
 
     conn.close()
+
+    ids_queue = sqs.get_queue_by_name(QueueName='url_facebook_stats')
+    ids_queue.purge()
+    logging.info('Purged queue for urls')
+
+    with psycopg2.connect(host=config['database']['host'],
+                          dbname=config['database']['db_name'],
+                          user=config['database']['user'],
+                          password=config['database']['password']) as conn_urls:
+        with conn_urls.cursor(name="cursor_urls", cursor_factory=extras.RealDictCursor) as cursor_urls:
+            logging.info('Will start SQL query')
+            cursor_urls.execute("""
+                select
+                  url.project_name,
+                  url.url
+                from
+                  url,
+                  project
+                where
+                  url.project_name = project.project_name and
+                  project.active and
+                  url.url not like '%.facebook.com%' and
+                  url.status_code <> 601 and
+                  not exists(
+                    select *
+                    from
+                      url_facebook_stats
+                    where
+                      url_facebook_stats.url = url.url and
+                      url_facebook_stats.project_name = url.project_name
+                    );    
+            """)
+            logging.info('Finished query')
+            no_more_results = False
+            while not no_more_results:
+                urls = cursor_urls.fetchmany(size=100)
+                if len(urls) == 0:
+                    no_more_results = True
+                    logging.info('No more results')
+                else:
+                    ids_queue.send_message(MessageBody=json.dumps(urls))
+                    logging.info('Sent message with %d URLs', len(urls))
+
+    logging.info('Disconnected from database')
+
+
+if __name__ == '__main__':
+    main()
